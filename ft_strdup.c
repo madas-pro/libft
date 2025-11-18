18c867bfd27c8c33f90c0c1f38e5a8c2bda16502
Adam Olivier <adolivie@f6r11s9.clusters.42paris.fr>
2025-11-18 12:44:20 +0100

    added ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,4 +1,43 @@
-char *strdup(const char *string)
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
 {
-    
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *string)
+{
+	char	*ptr;
+	int	i;
+
+	ptr = malloc(ft_strlen((char *)string) + 100);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (string[i])
+	{
+		ptr[i] = string[i];
+		i++;
+	}
+	ptr[i] = '\0';
+    return (ptr);
+}
+
+int	main(void)
+{
+	char	*string;
+	char	*newstr;
+
+	string = "this is a copy";
+	/* Make newstr point to a duplicate of string*/
+	if ((newstr = ft_strdup(string)) != NULL)
+		printf("The new string is: %s\n", newstr);
+	return (0);
+    free(newstr);
 }
