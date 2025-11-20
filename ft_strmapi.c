6e6c9f9a3630236d78ce196b0c73253702064d55
Adam Olivier <adolivie@f1r9s18.clusters.42paris.fr>
2025-11-20 23:27:36 +0100

    added ft_strmapi.c v1 and ft_putchar_fd.c v1

---
+++ ft_strmapi.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adolivie <adolivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 22:34:39 by adolivie          #+#    #+#             */
+/*   Updated: 2025/11/20 23:18:41 by adolivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char    ft_oupper(unsigned int i, char c)
+{
+    (void)i;
+	if (c >= 'a' && c <= 'z')
+		c = c + 'A' - 'a';
+	return (c);
+}
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*ptr;
+
+	ptr = malloc(ft_strlen((char *)s) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ptr[i] = f(i, s[i]);
+		i++;
+	}
+    ptr[i] = '\0';
+	return (ptr);
+}
+
+int	main(void)
+{
+	char *str;
+
+	str = ft_strmapi("Abcdef", ft_oupper);
+	printf("%s\n", str);
+    
+	free(str);
+}
