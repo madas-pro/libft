02a80076762039829a8caedff718b1e47b282cb9
Adam Olivier <adolivie@f1r9s18.clusters.42paris.fr>
2025-11-21 01:31:10 +0100

    added ft_putendl_fd.c v1 and ft_putnbr_fd.c v1

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adolivie <adolivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/21 01:08:03 by adolivie          #+#    #+#             */
+/*   Updated: 2025/11/21 01:30:11 by adolivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+	}
+	if (n > 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+	}
+	ft_putchar_fd(n % 10 + '0', fd);
+}
+
+int	main(void)
+{
+	ft_putnbr_fd(2147483647, 1);
+	ft_putnbr_fd(-2147483648, 1);
+	ft_putnbr_fd(2, 1);
+	ft_putnbr_fd(0, 1);
+	ft_putnbr_fd(-1, 1);
+	ft_putnbr_fd(-45, 1);
+	ft_putnbr_fd(-0, 1);
+}
